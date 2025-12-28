85c49e53a9234bd6a7fd790c62fbfff62c2d974c
sgjeon <ftatp5901@gmail.com>
2025-12-28 05:49:39 +0000

    Fixed slora to compute batchsize 256

--- slora/csrc/bgmv/bgmv_config.h
+++ slora/csrc/bgmv/bgmv_config.h
@@ -41,6 +41,7 @@ void bgmv_kernel(T *__restrict__ Y, const T *__restrict__ X,
     FOR_BGMV_WIDE(f, T, 16) \
     FOR_BGMV_WIDE(f, T, 32) \
     FOR_BGMV_WIDE(f, T, 64) \
-    FOR_BGMV_WIDE(f, T, 128)
+    FOR_BGMV_WIDE(f, T, 128) \
+    FOR_BGMV_WIDE(f, T, 256)
 
 // clang-format on
